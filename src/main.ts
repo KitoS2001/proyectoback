cb8971ac3a099feb4e101f32d2a8ebd31ca7bd43
Yahir Hernandez Hernandez <20211035@UTHH.EDU.MX>
2024-02-10 15:12:07 -0600

    oficial

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const CorsOptions: CorsOptions = {
-    origin:[ 'http://localhost:4200','https://proyectoclinicaback-back-production.up.railway.app/'],
+    origin:[ 'http://localhost:4200','https://proyectoclinicaback-back-production.up.railway.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   }
