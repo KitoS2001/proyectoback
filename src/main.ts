795da0d220a36b31c039bfa8d65b144013476529
Yahir Hernandez Hernandez <20211035@UTHH.EDU.MX>
2024-02-11 11:45:36 -0600

    oficial

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const CorsOptions: CorsOptions = {
-    origin:[ 'https://proyectoclinicaback-back-production.up.railway.app','https://65c8fd52cd4bfb3f24308df1--verdant-halva-9d1a43.netlify.app/'],
+    origin:[ 'https://proyectoclinicaback-back-production.up.railway.app','https://clincicadental.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   }
