680013a0bd5f85ca72d775feb41edd606f4ce719
Yahir Hernandez Hernandez <20211035@UTHH.EDU.MX>
2024-03-26 16:30:28 -0600

    26 Marzo

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { RecuperarPassModule } from './recuperar-pass/recuperar-pass.module';
   imports: [
     TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'roundhouse.proxy.rlwy.net',//localhost
+    host: 'roundhouse.proxy.rlwy.net',
     port: 3306,
     username: 'root',
     password: '',
